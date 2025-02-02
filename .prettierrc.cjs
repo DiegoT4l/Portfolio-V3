497f21587ef84dc9dfa9e20791b1afc232898c50
DiegoT4l <lopez.diiego.107@hotmail.com>
2025-02-02 12:56:24 -0600

    feat: add Prettier configuration and ignore file for consistent code formatting

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Config} */
+
+module.exports = {
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  jsxSingleQuote: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+};
